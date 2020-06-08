093f1f977012da1af3baf32857af1f63f0dcf6f3
Смирнов Павел <cocacola76rus@outlook.com>
2020-06-08 03:03:11 +0300

    Добавил Парсер, добавил xls сериализацию.

---
+++ Parser/Parser/Class/DataINNcs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Parser.Class
+{
+    [Serializable]
+    public class DataINN
+    {
+
+
+        public string Company { get; set; }
+        public string INN { get; set; }
+        public string Manager { get; set; }
+        public string Address { get; set; }
+        public DataINN() { }
+        public DataINN(string INN)
+        {
+            Company = "";
+            this.INN = INN;
+            Manager = "";
+            Address = "";
+        }
+        public DataINN(string Company, string INN, string Manager, string Address)
+        {
+            this.Company = Company;
+            this.INN = INN;
+            this.Manager = Manager;
+            this.Address = Address;
+        }
+
+    }
+}
--- Parser/Parser/MainWindow.xaml.cs
+++ Parser/Parser/MainWindow.xaml.cs
@@ -2,46 +2,26 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Windows;
-
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using System;
+using System.Xml.Serialization;
 
 namespace Parser
 {
-    public class DataINN
-    {
+    
 
-
-        public string Компания { get; set; }
-        public string ИНН { get; set; }
-        public string Руководитель { get; set; }
-        public string Адресс { get; set; }
-        public string ОКВЭД { get; set; }
-        public DataINN() { }
-        public DataINN(string INN)
-        {
-            Компания = "";
-            ИНН = INN;
-            Руководитель = "";
-            Адресс = "";
-            ОКВЭД = "";
-        }
-        public DataINN(string Company, string INN, string Manager, string Address, string OKBED)
-        {
-            Компания = Company;
-            ИНН = INN;
-            Руководитель = Manager;
-            Адресс = Address;
-            ОКВЭД = OKBED;
-        }
-
-    }
     public partial class MainWindow : Window
     {
+     
         public MainWindow()
         {
                
         }
 
-        List<DataINN> Data = new List<DataINN>();
+        List<Parser.Class.DataINN> Data = new List<Parser.Class.DataINN>();
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog file = new OpenFileDialog();
@@ -49,19 +29,41 @@ namespace Parser
             File.Text = file.FileName;
             using (StreamReader str = new StreamReader(File.Text))
             {
-                Data.Add(new DataINN(str.ReadLine()));
+                Data.Add(new Parser.Class.DataINN(str.ReadLine()));
 
             }
-
-            INNGrid.ItemsSource = Data;
-
-
-
         }
-
         private void CollectData_Click(object sender, RoutedEventArgs e)
         {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = @"https://egrul.nalog.ru/index.html";
+            foreach (var c in Data)
+            {       
+                driver.FindElement(By.XPath(@".//div[@id='uni_text_1']/div/input[@id='query']")).SendKeys(c.INN);
+                driver.FindElement(By.XPath(@".//button[@id='btnSearch']")).Click();
+                Thread.Sleep(3000);
+                var CompanyName = driver.FindElements(By.XPath(".//div[@id='resultContent']/div/div[2]/a"));
+                c.Company = CompanyName[0].Text;
+                var InformationAboutCompany = driver.FindElements(By.XPath(".//div[@id='resultContent']/div/div[3]/div"));
+                string Information = InformationAboutCompany[0].Text;
+                c.Address = Information.Substring(0, Information.IndexOf(", ОГРН"));
+                c.Manager = Information.Substring(Information.IndexOf("Директор: ")+10);
+            }
+            driver.Close();
+            INNGrid.ItemsSource = Data;
+        }
 
+        private void Serialization_Click(object sender, RoutedEventArgs e)
+        {
+            XmlSerializer formatter = new XmlSerializer(typeof(Parser.Class.DataINN));
+            SaveFileDialog file = new SaveFileDialog();
+            file.ShowDialog();
+            foreach (var c in Data)
+            using (FileStream fs = new FileStream(file.FileName, FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, c);
+
+            }
         }
     }
 }
